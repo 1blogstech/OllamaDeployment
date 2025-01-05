cb7107d180da9428f2a2f5a8c3449682c3df6580
1blogstech <139560989+1blogstech@users.noreply.github.com>
2025-01-05 22:19:26 +0530

    Update ollama_app.py

--- ollama_app.py
+++ ollama_app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request, jsonify
-import chatbot
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 import datetime
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+
+model = OllamaLLM(model="llama3.2")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def handle_conversation(user_input):
+    context = ""
+    result = chain.invoke({"context":context,"question":user_input})
+    context = f"\nUser: {user_input}\nAI: {result}"
+    return result
 
 app = Flask(__name__, template_folder="templates")
 
@@ -13,7 +34,7 @@ def main_page():
 def chat_with_ollama():
     data = request.json  # Get JSON data from frontend
     user_message = data.get("message")  # Get user's message
-    ollama = chatbot.handle_conversation(user_message)
+    ollama = handle_conversation(user_message)
     time_ = datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")
     data = str(ollama)
     return jsonify({"response": data, "time": time_})
